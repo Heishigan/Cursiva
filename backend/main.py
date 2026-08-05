c3e2f6c7c996aed90a4f600c25a49f09d90bd4dc
unknown <pheishi@live.com>
2026-08-05 08:15:16 +0200

    fix: allow chrome extension cors origins

--- backend/main.py
+++ backend/main.py
@@ -57,9 +57,10 @@ ALLOWED_ORIGINS = [
 app.add_middleware(
     CORSMiddleware,
     allow_origins=ALLOWED_ORIGINS,
+    allow_origin_regex=r"chrome-extension://.*",
     allow_credentials=True,
-    allow_methods=["GET", "POST", "PUT", "DELETE"],
-    allow_headers=["Authorization", "Content-Type"],
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 # BYOK has been removed
